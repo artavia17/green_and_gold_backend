694de380aaa31612acfdae00b4e5105578df367d
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2023-11-03 18:35:10 -0600

    Se agrego el home slider

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -362,6 +362,38 @@ export interface AdminTransferTokenPermission extends Schema.CollectionType {
   };
 }
 
+export interface ApiHomeSliderHomeSlider extends Schema.CollectionType {
+  collectionName: 'home_sliders';
+  info: {
+    singularName: 'home-slider';
+    pluralName: 'home-sliders';
+    displayName: 'Home Slider';
+  };
+  options: {
+    draftAndPublish: true;
+  };
+  attributes: {
+    Background: Attribute.Media;
+    Title: Attribute.String;
+    Action: Attribute.Text;
+    createdAt: Attribute.DateTime;
+    updatedAt: Attribute.DateTime;
+    publishedAt: Attribute.DateTime;
+    createdBy: Attribute.Relation<
+      'api::home-slider.home-slider',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+    updatedBy: Attribute.Relation<
+      'api::home-slider.home-slider',
+      'oneToOne',
+      'admin::user'
+    > &
+      Attribute.Private;
+  };
+}
+
 export interface PluginUploadFile extends Schema.CollectionType {
   collectionName: 'files';
   info: {
@@ -687,6 +719,7 @@ declare module '@strapi/types' {
       'admin::api-token-permission': AdminApiTokenPermission;
       'admin::transfer-token': AdminTransferToken;
       'admin::transfer-token-permission': AdminTransferTokenPermission;
+      'api::home-slider.home-slider': ApiHomeSliderHomeSlider;
       'plugin::upload.file': PluginUploadFile;
       'plugin::upload.folder': PluginUploadFolder;
       'plugin::users-permissions.permission': PluginUsersPermissionsPermission;
