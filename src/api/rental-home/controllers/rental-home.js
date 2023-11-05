b7d1d5eac8ab93f7dc37621e861a8fc7cfc7c945
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2023-11-04 21:35:06 -0600

    Se agrego la seccion de renta de casa

--- src/api/rental-home/controllers/rental-home.js
+++ src/api/rental-home/controllers/rental-home.js
@@ -6,4 +6,19 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::rental-home.rental-home');
+// module.exports = createCoreController('api::rental-home.rental-home');
+
+
+// Reemplazamos el id con el slug al momento de buscar con el find-one
+module.exports = createCoreController('api::rental-home.rental-home', ({ strapi }) => ({
+
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        const entity = await strapi.db.query('api::rental-home.rental-home').findOne({
+            where: { slug: id }
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+    }
+}));
